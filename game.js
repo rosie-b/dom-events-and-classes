b862473318b8d63bdd700ec7f1820149b1d74d7a
Rosie Burkett <burkettroz@gmail.com>
2018-02-05 17:29:34 +1300

    add totals

--- game.js
+++ game.js
@@ -43,7 +43,18 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  var dotTotals = document.getElementsByClassName('board')[0].children
 
+  for (var i = 0; i < dotTotals.length; i++) {
+    if (dotTotals[i].classList.contains('blue')) {
+      totals.blue += 1
+    } else if (dotTotals[i].classList.contains('green')) {
+      totals.green += 1
+    } else if (dotTotals[i].classList.contains('invisible')) {
+      totals.invisible += 1
+    }
+  }
+  
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
